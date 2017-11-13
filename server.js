9a63f5c76e96cda812ff718556b0c31304afa77c
Adrien <verblodung@gmail.com>
2017-11-13 03:04:02 -0500

    fix(server); changing default port for deployment

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 3000
 
 app.use(express.static(__dirname + '/public'));
 
-const server = app.listen(3000, function() {
+const server = app.listen(PORT, function() {
   const host = server.address().address;
   const port = server.address().port;
   console.log('Example app listening at http://%s:%s', host, port);
